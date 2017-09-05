83abae81c22b37f3f95020ead55942f673bc9813
Gabriel <gmathias1717@gmail.com>
2017-09-05 09:23:29 -0300

    Initial Commit

---
+++ ProjProgVisual/ProjProgVisual/Controllers/CategoriesController.cs
@@ -0,0 +1,106 @@
+﻿using ProjProgVisual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjProgVisual.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private static IList<Category> categoryList =
+            new List<Category>()
+            {
+                new Category() { CategoryId = 1, Name = "Laptop" },
+                new Category() { CategoryId = 2, Name = "Monitor" },
+                new Category() { CategoryId = 3, Name = "Keyboard" },
+                new Category() { CategoryId = 4, Name = "Mouse" },
+                new Category() { CategoryId = 5, Name = "Cabos" },
+            };
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(categoryList);
+        }
+
+        #region Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category)
+        {
+            categoryList.Add(category);
+            category.CategoryId =
+                categoryList.Max(c => c.CategoryId) + 1;
+            return RedirectToAction("Create");
+        }
+        #endregion
+
+        #region Edit
+        public ActionResult Edit(long id)
+        {
+            var category = categoryList
+                .Where(c => c.CategoryId == id)
+                .First();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category modified)
+        {
+            var category = categoryList
+                .Where(c => c.CategoryId == modified.CategoryId)
+                .First();
+
+            category.Name = modified.Name;
+
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Details
+        public ActionResult Details(long id)
+        {
+            var category = categoryList
+                .Where(c => c.CategoryId == id)
+                .First();
+
+            return View(category);
+        }
+        #endregion
+
+        #region Delete
+        public ActionResult Delete(long id)
+        {
+            var category = categoryList
+                .Where(c => c.CategoryId == id)
+                .First();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Category toDelete)
+        {
+            var category = categoryList
+                .Where(c => c.CategoryId == toDelete.CategoryId)
+                .First();
+
+            categoryList.Remove(category);
+
+            return RedirectToAction("Index");
+
+        }
+        #endregion
+    }
+}
---
+++ ProjProgVisual/ProjProgVisual/Controllers/SuppliersController.cs
@@ -0,0 +1,87 @@
+﻿using ProjProgVisual.Context;
+using ProjProgVisual.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjProgVisual.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly EFContext _context = new EFContext();
+        private object context;
+
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            return View(_context
+                .Suppliers
+                .OrderBy(s => s.Name));
+        }
+
+        #region Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Supplier supplier)
+        {
+            _context.Suppliers.Add(supplier);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Edit
+        // GET: Suppliers/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (!id.HasValue)
+            {
+
+                return new
+                    HttpStatusCodeResult(
+                        HttpStatusCode.BadRequest);
+            }
+
+            var supplier = _context
+                .Suppliers
+                .Find(id.Value);
+
+            if (supplier == null)
+            {
+
+                return new
+                    HttpStatusCodeResult(
+                        HttpStatusCode.NotFound);
+            }
+            return View(supplier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(supplier)
+                    .State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View(supplier);
+        }
+
+
+        #endregion
+    }
+}
